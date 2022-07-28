4b1acac5a3deb005dfea523029ca6406e65bce5c
ThanDieuDaiHiep <a0333087345@gmail.com>
2022-07-28 18:38:34 +0700

    upload mongodb deploy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,7 +6,9 @@ import { Song, SongSchema } from './schema/Music.shema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/Learning'),
+    MongooseModule.forRoot(
+      'mongodb+srv://hiepeng:Abcd1234.@clusterhiep.3zy5yn8.mongodb.net/fullstack',
+    ),
     MongooseModule.forFeature([{ name: Song.name, schema: SongSchema }]),
   ],
   controllers: [AppController],
